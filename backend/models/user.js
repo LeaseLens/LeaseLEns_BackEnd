d91cf4e8dbfea3d1b23099ef7c84eb09c4da6c90
Joonyoung <blackhound97@gmail.com>
2024-06-18 13:46:06 +0900

    FEAT : sequelize 데이터베이스 생성 중간커밋

---
+++ backend/models/user.js
@@ -0,0 +1,42 @@
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+
+module.exports = class User extends Model{
+  static init(sequelize){
+    return super.init(
+      {
+        user_index: {
+          type: DataTypes.INTEGER,
+          allowNull:false,
+          autoIncrement:true,
+          primaryKey:true,
+        },
+        user_ID:{
+          type: DataTypes.STRING,
+          allowNull:false,
+          unique:true,
+        },
+        user_name:{
+          type:DataTypes.STRING,
+          allowNull:false,
+        },
+        user_points:{
+          type: DataTypes.INTEGER,
+          allowNull:false,
+          defaultValue:0
+        },
+      },
+      {
+        sequelize,
+        modelName:'User',
+        tableName:'users',
+        charset:'utf8mb4',
+        collate:'utf8mb4_general_ci',
+      }
+    );
+  }
+static associate(db){
+  db.User.hasMany(db.Review)
+  db.User.hasMany(db.Comment)
+}
+}
