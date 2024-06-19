220c230c8b996788eb60891d59a4dd94184b5f4f
ideaspace785 <121221475+ideaspace785@users.noreply.github.com>
2024-06-19 12:45:13 +0900

    Refactor: user.js

--- Test/SignInTest/src/models/user.js
+++ Test/SignInTest/src/models/user.js
@@ -1,22 +1,46 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('../sequelize');
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
 
-const User = sequelize.define('User', {
-    user_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    user_ID: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-}, {
-    timestamps: false
-});
-
-module.exports = User;
+module.exports = class User extends Model{
+  static init(sequelize){
+    return super.init(
+      {
+        user_index: {
+          type: DataTypes.INTEGER,
+          allowNull:false,
+          autoIncrement:true,
+          primaryKey:true,
+        },
+        user_ID:{
+          type: DataTypes.STRING(10),
+          allowNull:false,
+          unique:true,
+        },
+        user_name:{
+          type:DataTypes.STRING(10),
+          allowNull:false,
+        },
+        user_pw:{
+          type:DataTypes.STRING(255),
+          allowNull:false,
+        },
+        user_points:{
+          type: DataTypes.INTEGER,
+          allowNull:false,
+          defaultValue:0
+        },
+      },
+      {
+        sequelize,
+        modelName:'User',
+        tableName:'users',
+        charset:'utf8mb4',
+        collate:'utf8mb4_general_ci',
+      });
+  }
+static associate(db){
+  db.User.hasMany(db.Review, {foreignKey:'user_index'});
+  db.User.hasMany(db.Comment, {foreignKey:'user_index'});
+  db.User.belongsToMany(db.Product, {through:'favorites' , foreignKey:'user_index'})
+  }
+}
