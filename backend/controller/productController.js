4d9f61ce9fd744a3241eb907176d35969346a69b
Joonyoung <blackhound97@gmail.com>
2024-06-21 00:56:14 +0900

    FEAT : 제품 페이지 렌더링 구현

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,17 +1,28 @@
 const {Op} = require('sequelize');
 const {comment, product, review, user} = require('../models');
 //제품 페이지 렌더링
-exports.main = async(req,res) => {
+exports.main = async (req,res) => {
   try{
-    //제품 목록
-    const products = product.findAll();
+    //category 쿼리 가져오기
+    const category = req.query.category || 'all';
+
+    //검색 조건 설정하기
+    const condition = category === 'all' ? {} : {prod_category : category};
+
+    //제품 목록 가져오기
+    const products = product.findAll({
+      where : condition,
+      attributes: ['prod_index', 'prod_img', 'prod_name', 'prod_likes', 'prod_price'] // 필요한 필드 목록
+    
+    });
 
     //요청이 성공한 경우
   res.json(products);
   }catch(err){
-
-  }
-  
+    //요청이 실패한 경우 에러처리
+    console.error(err);
+    res.status(500).send('서버 오류가 발생하였습니다.');
+  } 
 }
 
 //제품 상세 페이지
