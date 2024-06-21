bc928d04e14d3c995b7c265ad7792327430a36bc
Joonyoung <blackhound97@gmail.com>
2024-06-21 11:15:12 +0900

    RENAME : 병합을 위한 임시커밋

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -2,15 +2,13 @@ const express =require('express');
 const router = express.Router();
 const controller = require('../controller/productController')
 
-//제품 페이지 렌더링
+//제품 페이지 조회 및 검색
 router.get('/', controller.main);
 
 //제품 상세 페이지
 router.get('/:prod_idx',controller.details);
 
-//제품 검색하기
-router.get('/search',controller.search);
-
 //제품 찜하기
 router.post('/:prod_idx/like',controller.like);
+
 module.exports = router;
