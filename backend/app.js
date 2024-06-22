d93953f6a585bd68ccc57a8bf9fe8f945a4edabd
ideaspace785 <121221475+ideaspace785@users.noreply.github.com>
2024-06-22 10:19:28 +0900

    REFACTOR: session 관련 버그 수정

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,6 @@ const error404 = require('./Middlewares/error404');
 const handleError = require('./Middlewares/handleError');
 const passportConfig=require('./passport');
 
-
 const {userRouter, productRouter,renderRouter,reviewRouter}= require('./routes');
 
 const PORT = 8080;
@@ -21,22 +20,24 @@ passportConfig(); //passport config 초기화
 //cookie parser를 활용하여 쿠키 해석하기
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(cookieParser());
 
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/users', userRouter);
 app.use('/reviews',reviewRouter);
 app.use('/products', productRouter);
 app.use('/', renderRouter);
 
-//express-session 미들웨어를 사용하여 세션 관리
 app.use(session({
-  secret: 'mysecret', // 세션 암호화에 사용될 키
-  resave:false,
-  saveUninitialized:true
-}))
+    secret:process.env.COOKIE_SECRET,
+    resave:false,
+    saveUninitialized:false,
+    cookie:{ secure:false }, //HTTPS 사용할 때 값을 true로 바꿀것
+
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //404 에러처리 미들웨어
 app.use(error404);
